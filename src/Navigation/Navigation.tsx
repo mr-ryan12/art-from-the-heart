11c21fd6868867f457cc7ea85b9c666f40ca0a80
Tony <tonycarpenter@gmail.com>
2022-02-19 14:38:05 -0500

    Enable css for navigation bar and category buttons

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -4,9 +4,18 @@ import './Navigation.scss'
 const Navigation = () => {
   return (
     <nav className="navigation-bar">
-      some stuff 
+      <h1 className="logo">
+        <div>
+          Art From The 
+        </div>
+        <div className="logo-accent">
+          Heart
+        </div>
+      </h1>
     </nav>
   )
 }
+
+
 
 export default Navigation
