62a678f9c63962a53d8307a35469efca346b1927
Jacob Quintana <jacobquintaa@gmail.com>
2022-02-19 19:57:51 -0700

    Add link styling

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Navigation.scss'
+import {Link} from 'react-router-dom'
 
 const Navigation = () => {
   return (
     <nav className="navigation-bar">
       <h1 className="logo">
-        <div>
-          Art From The 
-        </div>
-        <div className="logo-accent">
-          Heart
-        </div>
+        <Link className='title-link' to='/' >
+          <div className='art-from-the'>
+            Art From The 
+          </div>
+          <div className="logo-accent">
+            Heart
+          </div>
+        </Link>
       </h1>
     </nav>
   )
