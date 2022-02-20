621aca2bde095f3e9f2e61c7de7c4d9826f15dca
Ryan McBride <mr.ryan444@gmail.com>
2022-02-20 12:06:11 -0700

    Create new property for single art piece

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import Categories from './Categories/Categories'
-import Navigation from './Navigation/Navigation'
+import Categories from './Categories/Categories';
+import Navigation from './Navigation/Navigation';
 import { getArtDetails} from './apiCalls';
 import './App.scss';
-import ArtView from './ArtView/ArtView'
-import {Route, Link, Switch} from 'react-router-dom'
+import ArtView from './ArtView/ArtView';
+import {Route, Switch} from 'react-router-dom';
 
 
 interface State {
   artPieces: Props[];
-  imgId: string
+  imgId: string;
+  singleImageDetails: object | undefined;
 }
 
 interface Props {
@@ -28,38 +29,8 @@ class App extends Component<{}, State> {
     super(props)
     this.state = {
       imgId: '',
-      artPieces: [
-        // {
-        //   id: 1,
-        //   image_src: 'https://www.artic.edu/iiif/2/1adf2696-8489-499b-cad2-821d7fde4b33/full/843,/0/default.jpg',
-        //   description: 'Some Description',
-        //   title: 'A really pretty painting',
-        //   date_start: 2009,
-        //   image_id: '239832',
-        //   artist_title: 'Someone',
-        //   classification_titles: ['bronze', 'metal', 'sculpture', 'modern and contemporary art']
-        // },
-        // {
-        //   id: 2,
-        //   image_src: 'https://www.artic.edu/iiif/2/65272293-e9c9-fc2b-887c-0e9687ec4221/full/843,/0/default.jpg',
-        //   description: 'Some other description',
-        //   title: 'Another amazing thing',
-        //   date_start: 1844,
-        //   image_id: '2789832',
-        //   artist_title: 'Someone else',
-        //   classification_titles: ['sculpture', 'figure']
-        // },
-        // {
-        //   id: 3,
-        //   image_src: 'https://www.artic.edu/iiif/2/6bec08f2-cb32-6bd4-54f0-6ac8cb68c218/full/843,/0/default.jpg',
-        //   description: 'Some other other description',
-        //   title: 'Another something else',
-        //   date_start: 1458,
-        //   image_id: '291832',
-        //   artist_title: 'Someone someone else',
-        //   classification_titles: ['painting', 'modern and contemporary art']
-        // }
-      ]
+      artPieces: [],
+      singleImageDetails: {}
     }
   }
 
@@ -70,6 +41,8 @@ class App extends Component<{}, State> {
       this.setState({artPieces: data.data})
       let randomImage = this.state.artPieces[this.getRandomIndex(this.state.artPieces.length)]['image_id']
       this.getImage(randomImage)
+      let foundPiece = this.state.artPieces.find(piece => piece.image_id === randomImage)
+      this.setState({singleImageDetails: foundPiece})
     })
   }
 
@@ -80,11 +53,12 @@ class App extends Component<{}, State> {
   
   getImage = (image: string) => {
     this.setState({imgId: image})
-    }
+  }
 
   render() {
     return (
       <>
+      {console.log(this.state.singleImageDetails)}
         <Navigation />
         <main className="App">
           < Route exact path='/' render={() => <Categories getCategory={this.getCategory} imageId={this.state.imgId} /> }/>
