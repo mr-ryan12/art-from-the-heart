0d78c02ef11c9c7a1a2810e4a416d234a13e3259
Ryan McBride <mr.ryan444@gmail.com>
2022-02-20 17:17:27 -0700

    Style art details container

--- src/ArtDetails/ArtDetails.tsx
+++ src/ArtDetails/ArtDetails.tsx
@@ -1,21 +1,6 @@
-import React from 'react'
-import './ArtDetails.scss'
-import '../ArtView/ArtView.scss'
-
-// {
-//   "_score": 92.88719,
-//   "thumbnail": {
-//     "alt_text": "A work made of gelatin silver print.",
-//     "width": 1281,
-//     "lqip": "data:image/gif;base64,R0lGODlhBwAFAPQAAD8/P0NDQ0ZGRmZmZnp6eoqKipGRkZmZmZ2dnZ6enqCgoKGhoaSkpKWlpaampqenp6ioqKmpqaqqqqurq6ysrK2tra+vr7Ozs7e3t7q6ugAAAAAAAAAAAAAAAAAAAAAAACH5BAAAAAAAIf4KQXBwbGVNYXJrCgAh/wtJbWFnZU1hZ2ljaw5nYW1tYT0wLjQ1NDU0NQAsAAAAAAcABQAABR2gJEFT1DCIcjBRYgTCkCkFAQwOVi2K0lwPisUSAgA7",
-//     "height": 907
-//   },
-//   "date_start": 2003,
-//   "date_end": 2004,
-//   "id": 202832,
-//   "image_id": "ad8092b2-f422-01b5-908a-e1911ce469b1",
-//   "title": "29 Palms: Security and Stabilization Operations, Graffiti I"
-//   },
+import React from 'react';
+import './ArtDetails.scss';
+import '../ArtView/ArtView.scss';
 
 interface Props {
   artDetails: Details;
@@ -38,22 +23,16 @@ interface Details {
 }
 
 const ArtDetails: React.FC<Props> = props => {
-  {console.log(props.artDetails)}
-
   return (
     <section className="art-details-container">
-      {/* <h2>{props.artDetails.title}</h2>
-      <h2>{props.artDetails.thumbnail}</h2> */}
-      <div className="centered">
-        <h2>{props.artDetails.title}</h2>
-        <p>{props.artDetails.date_start}</p>
-        <p>{props.artDetails.date_end}</p>
-        <p>{props.artDetails.artist_title}</p>
-        <p>{props.artDetails.thumbnail.alt_text}</p>
-
-      </div>
+      <h2>TITLE:</h2><p className="art-details-text">{props.artDetails.title}</p>
+      <h2>STARTING DATE:</h2><p className="art-details-text">{props.artDetails.date_start}</p>
+      <h2>ENDING DATE:</h2><p className="art-details-text">{props.artDetails.date_end}</p>
+      <h2>ARTIST:</h2><p className="art-details-text">{props.artDetails.artist_title}</p>
+      <h2>DESCRIPTION:</h2><p className="art-details-text">{props.artDetails.thumbnail.alt_text}</p>
+      <p className="percent-message">You and 78% of other voters liked this art! You have great taste!</p>
     </section>
   )
 }
 
-export default ArtDetails
+export default ArtDetails;
