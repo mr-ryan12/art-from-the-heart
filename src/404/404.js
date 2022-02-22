4b047bf3e95de3c935d2e7393dd645ba1b915973
Tony <tonycarpenter@gmail.com>
2022-02-22 13:18:53 -0500

    Add 404 error styling and gif

--- src/404/404.js
+++ src/404/404.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './404.css'
 
 const ErrorHandling = () => {
   return (
-    <section className="404error">
-      <h1>404 not found! Something went wrong.</h1>
+    <section className="error">
+      <img src="https://media2.giphy.com/media/rYEAkYihZsyWs/giphy.gif?cid=ecf05e47mxw0p2ev880va40mrexkbc8mm0gvmprvp8elek15&rid=giphy.gif&ct=g" alt="Bob Ross 404 error"/>
+      <h1 class="error-message">Page not found. Let's just paint a happy little 404 message right here!</h1>
     </section>
   )
 }
