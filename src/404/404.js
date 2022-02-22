25490151fc9fb019ac57de8848a237b77b484328
Jacob Quintana <jacobquintaa@gmail.com>
2022-02-22 10:47:15 -0700

    Start error handling

---
+++ src/404/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Loading.scss';
+
+const errorHandling = () => {
+  return (
+    <section className="404error">
+      <h1>404 not found! Something went wrong.</h1>
+    </section>
+  )
+}
+
+export default errorHandling;
