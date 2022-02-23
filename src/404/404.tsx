aaec4bb498417a69c7265bdb10d1f8dbfe63c5b5
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-22 18:44:20 -0700

    Add classname to image in error handler

--- src/404/404.tsx
+++ src/404/404.tsx
@@ -9,7 +9,7 @@ interface EProps {
 const ErrorHandling: React.FC<EProps> = ({message}) => {
   return (
     <section className="error">
-      <img src="https://media2.giphy.com/media/rYEAkYihZsyWs/giphy.gif?cid=ecf05e47mxw0p2ev880va40mrexkbc8mm0gvmprvp8elek15&rid=giphy.gif&ct=g" alt="Bob Ross 404 error"/>
+      <img className='error-pic' src="https://media2.giphy.com/media/rYEAkYihZsyWs/giphy.gif?cid=ecf05e47mxw0p2ev880va40mrexkbc8mm0gvmprvp8elek15&rid=giphy.gif&ct=g" alt="Bob Ross 404 error"/>
       <h1 className="error-message">{message}</h1>
       <NavLink to="/" className='return-to-main'>Back To Main Page</NavLink>
     </section>
