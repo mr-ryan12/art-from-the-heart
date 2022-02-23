1fc8492f5dd96e8c71430b5f8624a565cb2790e8
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-22 17:59:01 -0700

    Rename classes for specific css styling

--- src/404/404.tsx
+++ src/404/404.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './404.scss'
+
 interface EProps {
   message: string
 }
@@ -9,6 +11,7 @@ const ErrorHandling: React.FC<EProps> = ({message}) => {
     <section className="error">
       <img src="https://media2.giphy.com/media/rYEAkYihZsyWs/giphy.gif?cid=ecf05e47mxw0p2ev880va40mrexkbc8mm0gvmprvp8elek15&rid=giphy.gif&ct=g" alt="Bob Ross 404 error"/>
       <h1 className="error-message">{message}</h1>
+      <NavLink to="/" className='return-to-main'>Back To Main Page</NavLink>
     </section>
   )
 }
