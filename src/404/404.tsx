b2da9a69105e8d5f996db6e37f92d769ccaf4fbb
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-22 17:45:46 -0700

    Pass down prop to error handler component

--- src/404/404.tsx
+++ src/404/404.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './404.scss'
+interface EProps {
+  message: string
+}
 
-const ErrorHandling = () => {
+const ErrorHandling: React.FC<EProps> = ({message}) => {
   return (
     <section className="error">
       <img src="https://media2.giphy.com/media/rYEAkYihZsyWs/giphy.gif?cid=ecf05e47mxw0p2ev880va40mrexkbc8mm0gvmprvp8elek15&rid=giphy.gif&ct=g" alt="Bob Ross 404 error"/>
-      <h1 className="error-message">Page not found. Let's just paint a happy little 404 message right here!</h1>
+      <h1 className="error-message">{message}</h1>
     </section>
   )
 }
