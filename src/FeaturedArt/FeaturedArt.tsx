32bd62e1d54ad8caa9a258b6fa2935ee385f53ae
Ryan McBride <mr.ryan444@gmail.com>
2022-02-25 09:49:41 -0700

    Hide like buttons on click

--- src/FeaturedArt/FeaturedArt.tsx
+++ src/FeaturedArt/FeaturedArt.tsx
@@ -5,21 +5,23 @@ import './FeaturedArt.scss'
 
 interface Props {
   randomImageId: string;
-  findPiece: (value: boolean) => void
+  findPiece: (value: boolean) => void;
+  userLikesPhoto: boolean | undefined;
 }
 
 const FeaturedArt: React.FC<Props> = props => {
+  const showLikeButtons = props.userLikesPhoto === undefined ? <div className="user-choices">
+  <button className="ratings-choice" onClick={() => props.findPiece(true)}>
+    <img src={thumbsUpIcon} alt="I like this art" />
+  </button>  
+  <button className="ratings-choice" onClick={() => props.findPiece(false)}>
+    <img src={thumbsDownIcon} alt="I don't like this art" />
+  </button>  
+</div> : null;
   return (
     <>
     <img className="featured-art" src={`https://www.artic.edu/iiif/2/${props.randomImageId}/full/843,/0/default.jpg`} alt="beautiful artwork" />
-      <div className="user-choices">
-        <button className="ratings-choice" onClick={() => props.findPiece(true)}>
-          <img src={thumbsUpIcon} alt="I like this art" />
-        </button>  
-        <button className="ratings-choice" onClick={() => props.findPiece(false)}>
-          <img src={thumbsDownIcon} alt="I don't like this art" />
-        </button>  
-      </div>
+      {showLikeButtons}
     </>
   )
 }
