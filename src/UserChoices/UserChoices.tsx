7e909e9e65201efe180753dbde2816470014db17
Ryan McBride <mr.ryan444@gmail.com>
2022-02-26 15:27:19 -0700

    Add class name to user choices

--- src/UserChoices/UserChoices.tsx
+++ src/UserChoices/UserChoices.tsx
@@ -8,10 +8,10 @@ const UserChoices: React.FC<UserChoicesProps> = props => {
   return (
     <div className="user-choices">
       <button className="ratings-choice" onClick={() => props.findPiece(true)}>
-        <img src={thumbsUpIcon} alt="I like this art" />
+        <img src={thumbsUpIcon} alt="I like this art" className="icon"/>
       </button>  
       <button className="ratings-choice" onClick={() => props.findPiece(false)}>
-        <img src={thumbsDownIcon} alt="I don't like this art" />
+        <img src={thumbsDownIcon} alt="I don't like this art" className="icon"/>
       </button>  
     </div>
   )
