a564a2a558afb460e0d2ab8e58734c1173a9a86b
Tony <tonycarpenter@gmail.com>
2022-02-26 15:02:42 -0500

    Add angled accent on nav bar

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -6,18 +6,22 @@ import heartIcon from '../logo-heart-icon.png'
 const Navigation = () => {
   return (
     <nav className="navigation-bar">
-      <Link className='title-link' to='/' >
-        <h1 className="logo">
-          <div className='art-from-the'>
-            Art From The 
-          </div>
-          <div className="logo-accent">
-            Heart
-          </div>
-        </h1>
-        <img src={heartIcon} alt="Art From The Heart" className="logo-icon" />
-      </Link>
+      <div className="behind-logo">
+        <Link className='title-link' to='/' >
+          <h1 className="logo">
+            <div className='art-from-the'>
+              Art From The 
+            </div>
+            <div className="logo-accent">
+              Heart
+            </div>
+          </h1>
+          <img src={heartIcon} alt="Art From The Heart" className="logo-icon" />
+        </Link>
+      </div>
+      <div className="angled-end">
 
+      </div>
     </nav>
   )
 }
