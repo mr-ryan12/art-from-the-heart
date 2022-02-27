1b69e8b1271a6ed226f95b0de9bcf11440529d74
Tony <tonycarpenter@gmail.com>
2022-02-27 12:53:05 -0500

    Update animations for Featured Art View

--- src/FeaturedArt/FeaturedArt.tsx
+++ src/FeaturedArt/FeaturedArt.tsx
@@ -3,12 +3,18 @@ import UserChoices from '../UserChoices/UserChoices'
 // import '../ArtView/ArtView.scss'
 // import '../ArtDetails/ArtDetails.scss'
 import { FeaturedArtProps } from '../interface'
+import { motion } from "framer-motion"
 
 const FeaturedArt: React.FC<FeaturedArtProps> = props => {
   const showLikeButtons = props.userLikesPhoto === undefined ? <UserChoices findPiece={props.findPiece} /> : null
   return (
     <>
-      <img className="featured-art" src={`https://www.artic.edu/iiif/2/${props.randomImageId}/full/843,/0/default.jpg`} alt="beautiful artwork" />
+      <motion.img 
+        className="featured-art" src={`https://www.artic.edu/iiif/2/${props.randomImageId}/full/843,/0/default.jpg`} 
+        alt="beautiful artwork"
+        animate={{ opacity: [0 ,1] }}
+        transition={{ duration: .4 }}
+      />
       {showLikeButtons}
     </>
   )
