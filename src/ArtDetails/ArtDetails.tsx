01da4a5b89a3bfc7daa9b52b4fd30caa355fe647
Tony <tonycarpenter@gmail.com>
2022-02-27 12:51:37 -0500

    Update animations for Art Details

--- src/ArtDetails/ArtDetails.tsx
+++ src/ArtDetails/ArtDetails.tsx
@@ -3,12 +3,18 @@ import './ArtDetails.scss';
 import '../ArtView/ArtView.scss';
 import { Link } from 'react-router-dom'
 import { ArtDetailsProps } from '../interface';
+import { motion } from "framer-motion"
 
 const ArtDetails: React.FC<ArtDetailsProps> = props => {
   const likedMessage = props.isLiked ? <p className="percent-message">You and {Math.floor(Math.random() * 100) + 1 + '%'} of other voters liked this art! You have great taste!</p>
                                      : <p className="percent-message">You and {Math.floor(Math.random() * 100) + 1 + '%'} of other people agree! Congratulations!</p>
   return (
-    <section className="art-details-container">
+    <motion.section 
+      className="art-details-container"
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ duration: .4 }}
+    >
       <div className='main-details'>
         <h2 className='title-details'>TITLE:</h2><p className="art-details-title">{props.artDetails.title}</p>
         <h2 className='date-details'>STARTING DATE:</h2><p className="art-details-start">{props.artDetails.date_start}</p>
@@ -21,7 +27,7 @@ const ArtDetails: React.FC<ArtDetailsProps> = props => {
         <Link className='go-back-button' to={`/`}>Back To Main Page</Link>
         <button onClick={() => window.location.reload()} className='generate-new-button'>Show New Art Piece</button>
       </div>
-    </section>
+    </motion.section>
   )
 }
 
